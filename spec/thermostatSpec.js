477511c7e24e753537d33164ba3bf32a15c8169a
Paulo Azoia <pazoia.gomes@gmail.com>
2020-02-21 11:35:28 +0000

    Step 1 - Current temperature set to 20.

---
+++ spec/thermostatSpec.js
@@ -0,0 +1,19 @@
+/* 'use strict' 
+--> this make the testing more vigorous, 
+    there are certain allowances in non strict mode that will not throw errors.
+*/
+'use strict';
+
+describe('Thermostat', () => {
+  
+  var thermostat;
+
+  beforeEach(() => {
+    thermostat = new Thermostat();
+  });
+
+  it('starts at 20 degrees', () => {
+    expect(thermostat.temperature).toEqual(20);
+  });
+
+});
---
+++ src/thermostat.js
@@ -0,0 +1,9 @@
+/* 'use strict' 
+--> this make the testing more vigorous, 
+    there are certain allowances in non strict mode that will not throw errors.  
+*/
+'use strict';
+
+function Thermostat() {
+  this.temperature = 20;
+};
